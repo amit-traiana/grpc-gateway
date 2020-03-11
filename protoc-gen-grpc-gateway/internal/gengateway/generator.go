b6ce5362caddada9088a494c13e7ba1298410682
Amit Zarfati <irsp4m@gmail.com>
2020-03-11 17:26:25 +0200

    Bla

--- protoc-gen-grpc-gateway/internal/gengateway/generator.go
+++ protoc-gen-grpc-gateway/internal/gengateway/generator.go
@@ -3,7 +3,7 @@ package gengateway
 import (
 	"errors"
 	"fmt"
-	"go/format"
+	// "go/format"
 	"path"
 	"path/filepath"
 	"strings"
@@ -93,18 +93,18 @@ func (g *generator) Generate(targets []*descriptor.File) ([]*plugin.CodeGenerato
 	for _, file := range targets {
 		glog.V(1).Infof("Processing %s", file.GetName())
 		code, err := g.generate(file)
-		// if err == errNoTargetService {
-		// 	glog.V(1).Infof("%s: %v", file.GetName(), err)
-		// 	continue
+		if err == errNoTargetService {
+			glog.V(1).Infof("%s: %v", file.GetName(), err)
+			continue
+		}
+		// if err != nil {
+		// 	return nil, err
 		// }
-		if err != nil {
-			return nil, err
-		}
-		formatted, err := format.Source([]byte(code))
-		if err != nil {
-			glog.Errorf("%v: %s", err, code)
-			return nil, err
-		}
+		// formatted, err := format.Source([]byte(code))
+		// if err != nil {
+		// 	glog.Errorf("%v: %s", err, code)
+		// 	return nil, err
+		// }
 		name := file.GetName()
 		if g.pathType == pathTypeImport && file.GoPkg.Path != "" {
 			name = fmt.Sprintf("%s/%s", file.GoPkg.Path, filepath.Base(name))
