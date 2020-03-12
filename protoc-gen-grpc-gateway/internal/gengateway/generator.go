75dee9247b5ffc239edeb1b1e874c1937710eb73
Amit Zarfati <irsp4m@gmail.com>
2020-03-12 12:38:08 +0200

    bla

--- protoc-gen-grpc-gateway/internal/gengateway/generator.go
+++ protoc-gen-grpc-gateway/internal/gengateway/generator.go
@@ -97,6 +97,7 @@ func (g *generator) Generate(targets []*descriptor.File) ([]*plugin.CodeGenerato
 			glog.V(1).Infof("%s: %v", file.GetName(), err)
 			continue
 		}
+		fmt.Println("HI")
 		if err != nil {
 			return nil, err
 		}
@@ -117,7 +118,6 @@ func (g *generator) Generate(targets []*descriptor.File) ([]*plugin.CodeGenerato
 			Content: proto.String(string(formatted)),
 		})
 		glog.V(1).Infof("Will emit %s", output)
-		fmt.Println(output)
 	}
 	return files, nil
 }
