55dceacc0c3885df436bec225cacc08045169b85
Amit Zarfati <irsp4m@gmail.com>
2020-03-12 12:41:06 +0200

    bla

--- protoc-gen-grpc-gateway/internal/gengateway/generator.go
+++ protoc-gen-grpc-gateway/internal/gengateway/generator.go
@@ -91,9 +91,10 @@ func New(reg *descriptor.Registry, useRequestContext bool, registerFuncSuffix, p
 func (g *generator) Generate(targets []*descriptor.File) ([]*plugin.CodeGeneratorResponse_File, error) {
 	var files []*plugin.CodeGeneratorResponse_File
 	for _, file := range targets {
-		fmt.Println("HI")
 		glog.V(1).Infof("Processing %s", file.GetName())
+		fmt.Println("HI")
 		code, err := g.generate(file)
+		fmt.Println("BYE")
 		if err == errNoTargetService {
 			glog.V(1).Infof("%s: %v", file.GetName(), err)
 			continue
